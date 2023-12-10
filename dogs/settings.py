e06e1fe6b220b649ae30d721b01a312b77ec4ef3
x22185101 <145561403+x22185101@users.noreply.github.com>
2023-12-10 16:43:44 +0000

    Debug was changed to False

--- dogs/settings.py
+++ dogs/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'idpk^i%4#-m=-w96%7&63vmh)p-rakk+qwf&pr+96+yv&afov7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','94bc11ea8c8f4c19876d6d08debe454c.vfs.cloud9.eu-west-1.amazonaws.com', 'x22185101-cdos-app-env.eba-phserbpr.eu-west-2.elasticbeanstalk.com']
 
