441223f3625bb62abdf569589d06566defa9577c
x22185101 <145561403+x22185101@users.noreply.github.com>
2024-03-26 09:52:51 +0000

    User name API intergrated and error is fixed

--- animal_shelter/urls.py
+++ animal_shelter/urls.py
@@ -21,8 +21,8 @@ urlpatterns = [
     path('create_animal/', views.user_create_animal, name='user_create_animal'),
     path('update_animal/<int:animal_shelter_id>/', views.user_update_animal, name='user_update_animal'),
     path('delete_animal/<int:animal_shelter_id>/', views.user_delete_animal, name='user_delete_animal'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout')
+    #path('login/', auth_views.LoginView.as_view(), name='login'),
+    #path('logout/', auth_views.LogoutView.as_view(), name='logout')
 
     
 ]
